e1794af1d49039d8eec4ca6b02b0df075ab2f4b6
Serik Ayazbayev <jetheez@yandex.ru>
2023-04-27 22:21:09 +0600

    feat: inital commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PageContainer from "./components/PageContainer/PageContainer";
+
+function App() {
+  return (
+    <div className="page-container">
+      <PageContainer />
+    </div>
+  );
+}
+
+export default App;
